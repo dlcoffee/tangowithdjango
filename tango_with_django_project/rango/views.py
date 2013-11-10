6591376d0fea5efa77d506f50affe611c05691f3
Daniel Tea <daniel.l.tea@gmail.com>
2013-11-10 11:27:23 -0800

    basic website running

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hellow world!")
+    return HttpResponse("Rango says hello world!")
