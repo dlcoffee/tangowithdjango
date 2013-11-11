12e45dc204e58220cfbf2162095538a99c597bc0
Daniel Tea <daniel.l.tea@gmail.com>
2013-11-11 00:34:10 -0800

    added an index and an about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says hello world!<br> \
+    	                 <a href='/rango/about/'>About</a>")
+
+
+def about(request):
+    response = HttpResponse()
+    response.write("Rango says: Here is the about page.<br>")
+    response.write("<a href='/rango/'>Index</a>")
+
+    return response
